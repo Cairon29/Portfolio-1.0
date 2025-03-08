df0c9aa78a8685e70b4adff38642943504896195
Cairon29 <josewjjunco@gmail.com>
2025-03-07 19:15:21 -0500

    feat: Responsive design applied in all project

--- src/components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import './footer-styles.css'
-
-export const Footer = () => {
-  return (
-    <footer id="home-footer">
-        Footer comming soon
-    </footer>
-  )
-}
--- src/pages/projects/page-sections/Header.tsx
+++
@@ -1,5 +0,0 @@
-export const Header = () => {
-  return (
-    <header id='header'>Header</header>
-  )
-}
