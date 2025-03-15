9ac90c46b34154689c703a194f996d7f9d292a00
Cairon29 <josewjjunco@gmail.com>
2025-03-14 23:52:21 -0500

    feat: email form integration added

--- src/components/contactMeSection/ContactMeSection.tsx
+++ src/components/contactMeSection/ContactMeSection.tsx
@@ -1,48 +1,57 @@
-import { TbMessageChatbotFilled } from "react-icons/tb";
+import { useState } from "react";
+import { ContactMeSectionStructure } from "./ContactMeSectionStructure"
+import emailjs from '@emailjs/browser';
 
-import './contact-me-section-styles.css';
 export const ContactMeSection = () => {
+  interface EmailData {
+    email: string;
+    subject: string;
+    message: string;
+  }
+  const [ emailData, setEmailData ] = useState<EmailData>({
+    email: "",
+    subject: "",
+    message: ""
+  })
+
+  const hdlEmailData = {
+    hdlEmail: (e: React.ChangeEvent<HTMLInputElement>) => {
+      setEmailData((prev) => ({...prev, email: e.target.value}))
+    },
+    hdlSubject: (e: React.ChangeEvent<HTMLInputElement>) => {
+      setEmailData((prev) => ({...prev, subject: e.target.value}))
+    },
+    hdlMessage: (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setEmailData((prev) => ({...prev, message: e.target.value}))
+    },
+    hdlSubmmit: (e:React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (emailData.email && emailData.subject && emailData.message) {
+        const templateData = {
+          email: emailData.email,
+          subject: emailData.subject,
+          message: emailData.message
+        }
+        emailjs.send("service_4y3rlya","template_unh7h9j", templateData, 'ywDeEgGUrzFe5aJnX').then(
+          (result) => {
+            console.log(result.text)
+          })
+        setEmailData({
+          email: "",
+          subject: "",
+          message: ""
+        })
+      }
+      
+    }
+  }
+
   return (
-    <div>
-      <h2>Reach me out!</h2>
-      <section id="contact-me-section">
-        <article className='contact-me-article'>
-          <h3>Get in touch</h3>
-          <div className='contact-me-content'>
-            <p>
-              Do you have any bussines idea? Lets talk about it and make it real! I'm always open to new projects and ideas.
-            </p>
-            <div className='contact-me-icon'>
-              <TbMessageChatbotFilled size={125} />
-            </div>
-          </div>
-        </article>
-        <article className='contact-me-article'>
-          <form id='contact-me-form'>
-            <section className='input-group'>
-              <label htmlFor="email">Email</label>
-              <div className="input-wrapper">
-                <input type="email" id="email" name="email" required placeholder="email@example.com" />
-              </div>
-            </section>
-
-            <section className='input-group'>
-              <label htmlFor="subject">Subject</label>
-              <div className="input-wrapper">
-                <input type="text" id="subject" name="subject" required placeholder="ej: We are intersted on you!" />
-              </div>
-            </section>
-
-            <section className='input-group'>
-              <label htmlFor="message">Message</label>
-              <div className="input-wrapper">
-                <textarea id="message" name="message" required></textarea>
-              </div>
-            </section>
-            <button type="submit" id='contact-email-submit-button'>Send</button>
-          </form>
-        </article>
-      </section>
-    </div>
+    <>
+      <ContactMeSectionStructure
+        propsValues={emailData}
+        propsFunctions={hdlEmailData}
+      />
+    </>
   )
 }
