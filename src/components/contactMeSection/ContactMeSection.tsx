b8e9251c796b88a9a2bdbfc60bbe036769cb3aef
Cairon29 <josewjjunco@gmail.com>
2025-03-15 00:00:42 -0500

    feat: error boundary for tostify notifications added

--- src/components/contactMeSection/ContactMeSection.tsx
+++ src/components/contactMeSection/ContactMeSection.tsx
@@ -53,8 +53,19 @@ export const ContactMeSection = () => {
           theme: "dark",
           transition: Bounce
         });
+      } else {
+        toast.warn('Something went wrong', {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+          transition: Bounce,
+        });
       }
-      
     }
   }
 
